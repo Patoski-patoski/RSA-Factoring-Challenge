88ab7fbc6808cb5d0b23d69dd2d6998bc7085e34
patrick okafor <patrickpeters911@gmail.com>
2023-11-26 17:02:11 -0800

    task: Factorize as many numbers as possible into a product of two smaller numbers

--- factors.c
+++ factors.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#include <stdbool.h>
 
 /**
  * toFindP - a function to find the smallest positive divisor of a number > 1
@@ -14,11 +13,12 @@
 
 int toFindP(long int num)
 {
-	int i = 2;
+	int i;
 
-	if (num <= i)
+	if (num <= 2)
 		return (0);
 
+	i = 2;
 	while (i <= num)
 	{
 		if ((num % i) == 0)
@@ -33,6 +33,7 @@ int toFindP(long int num)
  * is divided by its smallest positive divisor greater than 1
  *
  * @num: the number to find the quotient for
+ * @p: the smallest divisor
  * Return: the quotient when num is divided by its smallest positive
  * divisor reater than 1, If num is less than or equal to 2, returns num.
  *
@@ -40,8 +41,10 @@ int toFindP(long int num)
 
 int toFindQ(long int num, int p)
 {
-	int q = num / p;
-	return (q);
+	if (num <= 2)
+		return (num);
+
+	return (num / p);
 }
 
 /**
@@ -57,25 +60,25 @@ int main(int argc, char *argv[])
 {
 	int p, q;
 	ssize_t nread;
-	FILE *fptr;
-	char *line;
-
 	long int n;
+	FILE *fptr;
+	char *line = NULL;
 	size_t len = 0;
 
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+		return (EXIT_FAILURE);
+	}
 	fptr = fopen(argv[1], "r");
 	if (fptr == NULL)
 	{
-		fprintf(stdout, " Unable to Open File\n");
-		exit(EXIT_FAILURE);
+		fprintf(stdout, " Unable to Open File: %s\n", argv[1]);
+		return (EXIT_SUCCESS);
 	}
-
-	line = NULL;
-
 	while ((nread = getline(&line, &len, fptr)) != -1)
 	{
-		char *token = NULL;
-		char *delim = "\n";
+		char *token = NULL, *delim = "\n";
 
 		token = strtok(line, delim);
 		while (token != NULL)
@@ -83,7 +86,6 @@ int main(int argc, char *argv[])
 			n = atoll(token);
 			p = toFindP(n);
 			q = toFindQ(n, p);
-
 			fprintf(stdout, "%ld=%d*%d\n", n, q, p);
 			token = strtok(NULL, delim);
 		}
@@ -93,9 +95,7 @@ int main(int argc, char *argv[])
 		free(line);
 		exit(EXIT_FAILURE);
 	}
-
-	puts("");
 	free(line);
 	fclose(fptr);
-	return (0);
+	return (EXIT_SUCCESS);
 }
